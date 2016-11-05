217b68e477a5f25b44da287c2977c040d09b9810
Tony Wang <towa4489@celsius.it.uu.se>
2016-11-05 09:10:38 +0100

    Refactoring and added some comments

--- GeneratePassword.py
+++ GeneratePassword.py
@@ -2,31 +2,30 @@ import sys
 import random
 import math
 
-WORDS_PATH = "../../../usr/share/lib/dict/words" #directory of dictionary, you have to execute this program from /home/user1337/SecureComputerSystemsGroup12 
-NUMBER_OF_WORDS = 4 #number of words in the password
+NUMBER_OF_WORDS_IN_PASSWORD = 4 #number of words in the password
 
+DICTIONARY_PATH = "../../../usr/share/lib/dict/words" #directory of dictionary, you have to execute this program from /home/user1337/SecureComputerSystemsGroup12 
 
+# The dictionary file is formatted as 'word1/n', 'word2\n' etc. so it looks like 
+# apple
+# cat
+# Michigan
+# ...
 
 def main():
     password = "" #initalize password
 
-    with open(WORDS_PATH) as file: #put all words from the dictionary in a list called words
-        words = file.read().splitlines()
+    with open(DICTIONARY_PATH) as file: #put all dictionary words from the dictionary in a list called dictionaryWords
+        dictionaryWords = file.read().splitlines()
 
-    for _ in range(NUMBER_OF_WORDS): #loop NUMBER_OF_WORDS times
-        password = password +  random.choice(words) + " " #take a random word from the dictionary and use it in the password
+    for _ in range(NUMBER_OF_WORDS_IN_PASSWORD): #loop NUMBER_OF_WORDS_IN_PASSWORD times. In this case 4 times to generate a password with 4 words
+        password = password +  random.choice(dictionaryWords) + " " #take a random word from the dictionary and use it in the password
 
     print(password)
-    print str(NUMBER_OF_WORDS) + " RANDOM words from word list of " + str(len(words)) + " words"
+    print str(NUMBER_OF_WORDS_IN_PASSWORD) + " RANDOM words from word list of " + str(len(dictionaryWords)) + " words"
+
     #TODO calculate entropy
-    #print "Entropy:" + str(round(math.log(math.pow(len(words),NUMBER_OF_WORDS), 
+    #print "Entropy:" + str(round(math.log(math.pow(len(dictionaryWords),NUMBER_OF_WORDS_IN_PASSWORD), 
         
-    
-        
-
-    
-
-
-
 if __name__ == "__main__":
     main()
