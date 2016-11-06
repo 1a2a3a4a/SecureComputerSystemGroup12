cd031d525a71ffba895170d34e60fc185a8d731e
Tony <tony.wang.4489@student.uu.se>
2016-11-06 15:36:34 +0100

    changed some comments

--- GeneratePassword.py
+++ GeneratePassword.py
@@ -4,7 +4,8 @@ import math
 
 NUMBER_OF_WORDS_IN_PASSWORD = 4 #number of words in the password
 
-DICTIONARY_PATH = "../../../usr/share/lib/dict/words" #directory of dictionary, you have to execute this program from /home/user1337/SecureComputerSystemsGroup12 
+#directory of dictionary, you have to execute this program from /home/user1337/SecureComputerSystemsGroup12 
+DICTIONARY_PATH = "../../../usr/share/lib/dict/words" 
 
 # The dictionary file is formatted as 'word1/n', 'word2\n' etc. so it looks like 
 # apple
@@ -14,17 +15,20 @@ DICTIONARY_PATH = "../../../usr/share/lib/dict/words" #directory of dictionary, 
 
 def main():
     password = "" #initalize password
-
-    with open(DICTIONARY_PATH) as file: #put all dictionary words from the dictionary in a list called dictionaryWords
+    
+    #put all dictionary words from the dictionary in a list called dictionaryWords
+    with open(DICTIONARY_PATH) as file: 
         dictionaryWords = file.read().splitlines()
 
-    for _ in range(NUMBER_OF_WORDS_IN_PASSWORD): #loop NUMBER_OF_WORDS_IN_PASSWORD times. In this case 4 times to generate a password with 4 words
-        password = password +  random.choice(dictionaryWords) + " " #take a random word from the dictionary and use it in the password
-
+    #loop NUMBER_OF_WORDS_IN_PASSWORD times. In this case 4 times to generate a password with 4 words
+    for _ in range(NUMBER_OF_WORDS_IN_PASSWORD): 
+        #take a random word from the dictionary and use it in the password
+        password = password +  random.choice(dictionaryWords) + " " 
+        
     print(password)
     print str(NUMBER_OF_WORDS_IN_PASSWORD) + " RANDOM words from word list of " + str(len(dictionaryWords)) + " words"
 
-    #TODO calculate entropy
+   
     entropyOld = round(math.log(math.pow(len(dictionaryWords),NUMBER_OF_WORDS_IN_PASSWORD),2),2)
     entropyNew = entropyOld / math.log(62,2)
     print "Entropy:" + str(entropyOld) + " bits"
